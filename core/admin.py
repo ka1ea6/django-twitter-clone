13c15d2d72171f8ab0db8c83bf0da439fee59427
ka1ea6 <kaleab77gulilat@gmail.com>
2023-04-17 12:26:34 +0300

    admin init and profile setup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Profile
 
 # Register your models here.
+
+
+class MyAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(Profile, MyAdmin)
