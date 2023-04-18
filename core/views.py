81a26cdc1760ce144047e8b68fb800715c9c5510
ka1ea6 <kaleab77gulilat@gmail.com>
2023-04-18 15:04:14 +0300

    working on the template(home)

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    return redirect('/home')
+
+
+def home(request):
+    return render(request, 'core/home/home.html')
